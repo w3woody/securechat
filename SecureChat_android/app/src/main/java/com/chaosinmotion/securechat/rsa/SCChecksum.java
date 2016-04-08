df2ad63a4ea428ff2609eb98f183a5c2c177c887
William Woody <woody@alumni.caltech.edu>
2016-04-07 21:17:08 -0400

    CRC8 checksum code

---
+++ SecureChat_android/app/src/main/java/com/chaosinmotion/securechat/rsa/SCChecksum.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2016. William Edward Woody
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+package com.chaosinmotion.securechat.rsa;
+
+/**
+ * Created by woody on 4/7/16.
+ */
+public class SCChecksum
+{
+	private static final byte POLYNOMIAL = 0x07;
+	private static SCChecksum shared;
+	private byte[] crc8Table;
+
+	private SCChecksum()
+	{
+		crc8Table = new byte[256];
+
+		for (int i = 0; i < 256; i++) {
+			byte rem = (byte)i;  /* remainder from polynomial division */
+			for (int j = 0; j < 8; j++) {
+				if (0 != (rem & 0x80)) {
+					rem <<= 1;
+					rem ^= POLYNOMIAL;
+				} else {
+					rem <<= 1;
+				}
+			}
+			crc8Table[i] = rem;
+		}
+
+	}
+
+	/**
+	 * Singleton constructor.
+	 * @return
+	 */
+	public static synchronized SCChecksum get()
+	{
+		if (shared == null) {
+			shared = new SCChecksum();
+		}
+		return shared;
+	}
+
+	/**
+	 * Calculate the CRC8 checksum. Note that this takes a starting CRC
+	 * value; that can be used to chain checksum values across multiple
+	 * buffers.
+	 * @param crc
+	 * @param buf
+	 * @return
+	 */
+	public byte calcCRC8(byte crc, byte[] buf)
+	{
+		for (int i = 0; i < buf.length; ++i) {
+			crc = crc8Table[0x0FF & (crc ^ buf[i])];
+		}
+		return crc;
+	}
+}
---
+++ SecureChat_android/app/src/test/java/com/chaosinmotion/securechat/ChecksumUnitTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2016. William Edward Woody
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+package com.chaosinmotion.securechat;
+
+import com.chaosinmotion.securechat.rsa.SCChecksum;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by woody on 4/7/16.
+ */
+public class ChecksumUnitTest
+{
+	@Test
+	public void testCRC8Vectors()
+	{
+		byte[] bytes = new byte[32];
+
+		for (int i = 0; i < 32; ++i) bytes[i] = 0;
+		assertTrue(0x00 == SCChecksum.get().calcCRC8((byte)0,bytes));
+
+		for (int i = 0; i < 32; ++i) bytes[i] = (byte)i;
+		assertTrue(0x06 == SCChecksum.get().calcCRC8((byte)0,bytes));
+
+		for (int i = 0; i < 32; ++i) bytes[i] = (byte)(31-i);
+		assertTrue(0xf6 == SCChecksum.get().calcCRC8((byte)0,bytes));
+
+		for (int i = 0; i < 32; ++i) bytes[i] = (byte)0xFF;
+		assertTrue(0x09 == SCChecksum.get().calcCRC8((byte)0,bytes));
+	}
+}
